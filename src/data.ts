3f891f5c29d650380b9b60a3b14b0dabd9abab4d
Volkyr <yatel.lara@gmail.com>
2023-09-01 22:59:04 -0300

    Add components, styles and data

---
+++ src/data.ts
@@ -0,0 +1,238 @@
+export const menu = [
+  {
+    id: 1,
+    title: "Main",
+    listItems: [
+      {
+        id: 1,
+        title: "Homepage",
+        url: "/",
+        icon: "home.svg",
+      },
+      {
+        id: 2,
+        title: "Profile",
+        url: "/users/1",
+        icon: "user.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Lists",
+    listItems: [
+      {
+        id: 1,
+        title: "Users",
+        url: "/users",
+        icon: "user.svg",
+      },
+      {
+        id: 2,
+        title: "Products",
+        url: "/products",
+        icon: "product.svg",
+      },
+      {
+        id: 3,
+        title: "Orders",
+        url: "/orders",
+        icon: "order.svg",
+      },
+      {
+        id: 4,
+        title: "Posts",
+        url: "/posts",
+        icon: "post2.svg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "General",
+    listItems: [
+      {
+        id: 1,
+        title: "Elements",
+        url: "/",
+        icon: "element.svg",
+      },
+      {
+        id: 2,
+        title: "Notes",
+        url: "/",
+        icon: "note.svg",
+      },
+      {
+        id: 3,
+        title: "Forms",
+        url: "/",
+        icon: "form.svg",
+      },
+      {
+        id: 4,
+        title: "Calendar",
+        url: "/",
+        icon: "calendar.svg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Maintenance",
+    listItems: [
+      {
+        id: 1,
+        title: "Settings",
+        url: "/",
+        icon: "setting.svg",
+      },
+      {
+        id: 2,
+        title: "Backups",
+        url: "/",
+        icon: "backup.svg",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Analytics",
+    listItems: [
+      {
+        id: 1,
+        title: "Charts",
+        url: "/",
+        icon: "chart.svg",
+      },
+      {
+        id: 2,
+        title: "Logs",
+        url: "/",
+        icon: "log.svg",
+      },
+    ],
+  },
+];
+
+export const topDealUsers = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/8405873/pexels-photo-8405873.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    username: "Abby Johnes",
+    email: "abby.j@gmail.com",
+    amount: "3.668",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    username: "Nahuel Netech",
+    email: "nahuel.n@gmail.com",
+    amount: "3.256",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    username: "Tobi Jett",
+    email: "tobi.jett@gmail.com",
+    amount: "2.998",
+  },
+  {
+    id: 4,
+    img: "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    username: "Jackson Alí",
+    email: "jackson.ali@gmail.com",
+    amount: "2.512",
+  },
+  {
+    id: 5,
+    img: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    username: "Giorken Motorman",
+    email: "giorken.motor@gmail.com",
+    amount: "2.134",
+  },
+  {
+    id: 6,
+    img: "https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    username: "Augusto Pepovich",
+    email: "augusto.p@gmail.com",
+    amount: "1.932",
+  },
+  {
+    id: 7,
+    img: "https://images.pexels.com/photos/874158/pexels-photo-874158.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    username: "Young Pitta",
+    email: "young.pitta@gmail.com",
+    amount: "1.560",
+  },
+];
+
+export const chartBoxUser = {
+  color: "#8884d8",
+  icon: "/userIcon.svg",
+  title: "Total users",
+  number: "11.238",
+  dataKey: "users",
+  percentage: 45,
+  chartData: [
+    { name: "Sun", users: 400 },
+    { name: "Mon", users: 600 },
+    { name: "Tue", users: 500 },
+    { name: "Wed", users: 700 },
+    { name: "Thu", users: 400 },
+    { name: "Fri", users: 500 },
+    { name: "Sat", users: 450 },
+  ],
+};
+
+export const chartBoxProduct = {
+  color: "skyblue",
+  icon: "/productIcon.svg",
+  title: "Total products",
+  number: "238",
+  dataKey: "products",
+  percentage: 21,
+  chartData: [
+    { name: "Sun", products: 400 },
+    { name: "Mon", products: 600 },
+    { name: "Tue", products: 500 },
+    { name: "Wed", products: 700 },
+    { name: "Thu", products: 400 },
+    { name: "Fri", products: 500 },
+    { name: "Sat", products: 450 },
+  ],
+};
+export const chartBoxRevenue = {
+  color: "teal",
+  icon: "/revenueIcon.svg",
+  title: "Total revenue",
+  number: "$56.432",
+  dataKey: "revenue",
+  percentage: -12,
+  chartData: [
+    { name: "Sun", revenue: 400 },
+    { name: "Mon", revenue: 600 },
+    { name: "Tue", revenue: 500 },
+    { name: "Wed", revenue: 700 },
+    { name: "Thu", revenue: 400 },
+    { name: "Fri", revenue: 500 },
+    { name: "Sat", revenue: 450 },
+  ],
+};
+export const chartBoxConversion = {
+  color: "gold",
+  icon: "/conversionIcon.svg",
+  title: "Total ratio",
+  number: "2.6",
+  dataKey: "ratio",
+  percentage: 12,
+  chartData: [
+    { name: "Sun", ratio: 400 },
+    { name: "Mon", ratio: 600 },
+    { name: "Tue", ratio: 500 },
+    { name: "Wed", ratio: 700 },
+    { name: "Thu", ratio: 400 },
+    { name: "Fri", ratio: 500 },
+    { name: "Sat", ratio: 450 },
+  ],
+};
